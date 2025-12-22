e6ff6a22293cd52ea8377f4d22f77006007d9cfd
minion6011 <mvittorio29@gmail.com>
2025-12-22 17:37:29 +0100

    Update accounting page, in particular the JS code

--- website/pages/accounting.js
+++ website/pages/accounting.js
@@ -19,12 +19,17 @@ if (window.self === window.top) {
     document.documentElement.classList.add("not-iframe");
 }
 // --- Variables
-let optionsSelect = [0, 0]; // struct 0(input!=null), 1(select!=defaultoption)
+let optionsSelect = [false, false]; // struct 0(input!=null), 1(select!=defaultoption)
 const customSelect = document.getElementById("select-options");
 const selectedOption = document.getElementById("selected-value");
 const selectedNumber = document.getElementById("selected-number");
 const selectedDesc = document.getElementById("selected-description");
 const buttonAddPay = document.getElementById("payment-button");
+const paymentsTable = document.getElementById("payments-table");
+
+const balanceTot = document.getElementById("balance");
+const lossTot = document.getElementById("loss");
+const profitTot = document.getElementById("profit");
 // --- Functions
 
 // Custom Select
@@ -48,6 +53,7 @@ function CheckAddPayment() {
 }
 
 async function AddPayment() {
+    optionsSelect = [false, false]; buttonAddPay.disabled = true;
     let dict = {"type":"add"};
     dict["profit"] = dict["loss"] = 0;
     dict[selectedOption.innerText.toLowerCase()] = Number(selectedNumber.value)
@@ -59,7 +65,26 @@ async function AddPayment() {
         body: JSON.stringify(dict),
     });
     if (req.status == 200) {
-        console.log("added");
+        let json = JSON.parse(await req.text());
+        AddPaymentHTML(dict["profit"], dict["loss"], dict["description"], json.date)
     }
     else throw new Error("Adding a payment returns a non-200 status code");
+}
+function AddPaymentHTML(profit, loss, description, date) {
+    // Update Financial Report
+    balanceTot.innerText = parseFloat(balanceTot.innerText) + (profit - loss); 
+    lossTot.innerText = "-" + (Number(lossTot.innerText.slice(1)) + loss);
+    profitTot.innerText = "+" + (Number(profitTot.innerText.slice(1)) + profit);
+    // Add Table element
+    trElement = document.createElement("tr");
+    trElement.innerHTML = `
+    <th><code>${date}</code></th>
+    <th class="profit">+${profit}</th>
+    <th class="loss">-${loss}</th>
+    <th mobile="0">${description}</th>
+    `;
+    paymentsTable.children[0].insertBefore(trElement, paymentsTable.children[0].children[1]);
+    // Resets Values
+    selectedOption.innerText = "Select Operation Type";
+    selectedNumber.value = "";
 }
