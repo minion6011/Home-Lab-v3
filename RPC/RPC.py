97381689f15ec433ee4f4cb65cae9737f884e416
minion6011 <mvittorio29@gmail.com>
2026-01-31 21:59:04 +0100

    Minor Bug fix, added return messages

--- RPC/RPC.py
+++ RPC/RPC.py
@@ -6,8 +6,6 @@ import time, threading
 CLIENT_ID = "app_id"
 
 rpc = Presence(CLIENT_ID)
-rpc.connect()
-
 app = Flask(__name__)
 CORS(app)
 
@@ -19,6 +17,11 @@ def cancelRPC():
 @app.route("/rpc", methods=["POST"])
 def set_rpc():
     data = request.json
+    if str(rpc.loop).find("running=False") != -1:
+        try:
+            rpc.connect()
+        except:
+            return {"message": "Server ON, Discord client OFF"}, 200
     rpc.update(
         activity_type = ActivityType.LISTENING,
         details=data["title"],
@@ -33,6 +36,6 @@ def set_rpc():
 
     taskReset = threading.Timer(data["duration"], cancelRPC)
     taskReset.start()
-    return {}, 200
+    return {"message": "Server ON, Discord client ON"}, 200
 
 app.run(host="127.0.0.1", port=8765)
