95869b38bab9d76d7fbcfcd68580de5aa4af2ef2
Minion6011 <mvittorio29@gmail.com>
2026-03-13 17:07:47 +0100

    Removed functions from home.js that are already included in default.js
    Removed theme loader, iframe check, and logged check functions.

--- website/pages/home.js
+++ website/pages/home.js
@@ -7,41 +7,6 @@ const endpoints = {
 }
 
 /* --- Functions --- */
-/* - Default - */
-// Theme Loader
-if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("light");
-}
-else {
-    document.body.classList.remove("light");
-}
-
-window.addEventListener("storage", (event) => {
-    if (event.newValue === "light") {
-        document.body.classList.add("light");
-    }
-    else if (event.newValue === "dark"){
-        document.body.classList.remove("light");
-    }
-});
-// Iframe Check
-if (window.self === window.top) {
-    document.documentElement.classList.add("not-iframe");
-}
-// Logged Check
-(function () { // change how fetch works
-    const originalFetch = window.fetch;
-    window.fetch = async function (...args) {
-        const response = await originalFetch(...args);
-        if (response.status === 401) {
-            window.parent.location.reload();
-        }
-        return response;
-    };
-})();
-/* - - */
-
-// --- Functions
 async function SendCommand() {
     await fetch(endpoints.sendCommand, {
         method: "POST",
