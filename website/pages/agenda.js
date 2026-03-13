e6e81c28aaf948ce090c16d66ad88c984736fc89
Minion6011 <mvittorio29@gmail.com>
2026-03-13 16:58:29 +0100

    Removed functions from agenda.js that are already included in default.js

--- website/pages/agenda.js
+++ website/pages/agenda.js
@@ -14,40 +14,6 @@ const endpoints = {
 }
 
 /* --- Functions --- */
-/* - Default - */
-// Theme Loader
-if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("light");
-}
-else {
-    document.body.classList.remove("light");
-}
-
-window.addEventListener("storage", (event) => { // This event is different from the defaults
-    if (event.newValue === "light") {
-        document.body.classList.add("light");
-    }
-    else if (event.newValue === "dark") {
-        document.body.classList.remove("light");
-    };
-});
-// Iframe Check
-if (window.self === window.top) {
-    document.documentElement.classList.add("not-iframe");
-};
-// Logged Check
-(function () { // change how fetch works
-    const originalFetch = window.fetch;
-    window.fetch = async function (...args) {
-        const response = await originalFetch(...args);
-        if (response.status === 401) {
-            window.parent.location.reload();
-        }
-        return response;
-    };
-})();
-/* - - */
-
 // Todo
 async function switchTodo(element) {
     element.children[1].checked = !element.children[1].checked;
