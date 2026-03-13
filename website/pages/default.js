da4d00f47b9cf0a60e8296941db432ec30e68234
Minion6011 <mvittorio29@gmail.com>
2026-03-13 09:40:38 +0100

    Create default.js
    Created default.js, which will be used for scripts that need to run on every page.

---
+++ website/pages/default.js
@@ -0,0 +1,32 @@
+/* - Default - */
+// Theme Loader
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light");
+}
+else {
+    document.body.classList.remove("light");
+}
+
+window.addEventListener("storage", (event) => {
+    if (event.newValue === "light") {
+        document.body.classList.add("light");
+    }
+    else if (event.newValue === "dark"){
+        document.body.classList.remove("light");
+    }
+});
+// Iframe Check
+if (window.self === window.top) {
+    document.documentElement.classList.add("not-iframe");
+}
+// Logged Check
+ // change how fetch works
+const originalFetch = window.fetch;
+    window.fetch = async function (...args) {
+    const response = await originalFetch(...args);
+    if (response.status === 401) {
+        window.parent.location.reload();
+    }
+    return response;
+};
+/* - - */
