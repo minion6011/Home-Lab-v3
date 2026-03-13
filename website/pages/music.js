1c74b260eaa950a894a3d954e52abbe59ee26f04
Minion6011 <mvittorio29@gmail.com>
2026-03-13 17:09:17 +0100

    Removed functions from music.js that are already included in default.js
    Removed theme loader and iframe check functions, along with custom fetch handling.

--- website/pages/music.js
+++ website/pages/music.js
@@ -69,44 +69,6 @@ const endpoints = {
 }
 
 /* --- Functions --- */
-/* - Default - */
-// Theme Loader
-if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("light");
-}
-else {
-    document.body.classList.remove("light");
-}
-
-window.addEventListener("storage", (event) => { // This event is different from the defaults
-    if (event.newValue === "light") {
-        document.body.classList.add("light");
-    }
-    else if (event.newValue === "dark") {
-        document.body.classList.remove("light");
-    }
-});
-// Iframe Check
-if (window.self === window.top) {
-    document.documentElement.classList.add("not-iframe");
-}
-// Logged Check
-(function () { // change how fetch works
-    const originalFetch = window.fetch;
-    window.fetch = async function (...args) {
-        try {
-            const response = await originalFetch(...args);
-            if (response.status === 401) {
-                window.parent.location.reload();
-            }
-            return response;
-        } catch (err) {
-            return null;
-        }
-    };
-})();
-/* - - */
-
 // Songs Player
 async function DeleteSong(value) {
     let idSong = value.parentElement.parentElement.dataset.songId;
