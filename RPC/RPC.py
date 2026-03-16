cd18d4c2bbe78ecd179fc17e37880841a953ec38
minion6011 <mvittorio29@gmail.com>
2026-03-16 13:15:50 +0100

    Refactor inside the 'RPC.py', improve RPC handling and connection logic.

--- RPC/RPC.py
+++ RPC/RPC.py
@@ -4,46 +4,64 @@ import time, threading, pypresence
 
 CLIENT_ID = "app_id"
 
-rpc = pypresence.Presence(CLIENT_ID)
 app = Flask(__name__)
 CORS(app)
+rpc = pypresence.Presence(CLIENT_ID)
+is_connected = False
 
-taskReset = None
+lock = threading.Lock()
+task_reset = None
 
 def cancelRPC():
-    try: rpc.clear()
-    except: rpc.close()
+    global is_connected
+    try:
+        with lock: # Wait for the lock to "open"
+            rpc.clear()
+    except Exception:
+        is_connected = False
 
 def setRPC(title: str, artist: str, img: str, duration: float):
+    global task_reset, is_connected
     try:
-        rpc.update(
-            activity_type = pypresence.ActivityType.LISTENING,
-            details=title,
-            state=artist,
-            large_image=img,
-            start=time.time(),
-            end=time.time()+duration
-        )
-        global taskReset
-        if taskReset:
-            taskReset.cancel()
+        with lock: # Wait for the lock to "open"
+            if not is_connected:
+                rpc.connect()
+                is_connected = True
+            
+            rpc.update(
+                activity_type=pypresence.ActivityType.LISTENING,
+                details=title,
+                state=artist,
+                large_image=img,
+                start=time.time(),
+                end=time.time() + duration
+            )
 
-        taskReset = threading.Timer(duration-1, cancelRPC)
-        taskReset.start()
-        return True
+            if task_reset:
+                task_reset.cancel()
+            
+            task_reset = threading.Timer(duration, cancelRPC)
+            task_reset.start()
+            return True
     except Exception as e:
+        print(f"Errore RPC: {e}")
+        is_connected = False
         return False
 
 @app.route("/rpc", methods=["POST"])
-def set_rpc():
+def handle_rpc():
     data = request.json
-    state = setRPC(data["title"], data['artist'], data['img'], data["duration"])
-    if not state:
-        try:
-            rpc.connect()
-            state = setRPC(data["title"], data['artist'], data['img'], data["duration"])
-        except Exception as e:
-            return {"error": f"Error: {str(e)}"}, 500
-    return {"message": "Server ON, Discord client ON"}, 200
+    
+    required = ["title", "artist", "img", "duration"]
+    if not all(k in data for k in required):
+        return {"error": "Missing info"}, 400
 
-app.run(host="127.0.0.1", port=8765)
+    success = setRPC(data["title"], data['artist'], data['img'], float(data["duration"]))
+    
+    if success:
+        return {"message": "Server ON, Discord ON"}, 200
+    else:
+        return {"error": "Server ON, Discord OFF"}, 500
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8765)
